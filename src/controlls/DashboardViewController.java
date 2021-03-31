36bec9279e55be903447f3334a4117c83ddc29ef
Shazil <sjamal@lakeheadu.ca>
2021-03-31 12:56:27 -0400

    list of each prof

--- src/controlls/DashboardViewController.java
+++ src/controlls/DashboardViewController.java
@@ -1,4 +1,37 @@
 package controlls;
 
-public class DashboardViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import models.Person;
+import models.Professor;
+import models.Student;
+import utilities.DBUtility;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class DashboardViewController implements Intializable {
+
+    @FXML
+    private Label studentsLabel;
+    @FXML
+    private ListView<Student> studentListView;
+    @FXML
+    private Label proffessorsLabel;
+    @FXML
+    private ListView<Professor>professorListView;
+    @FXML
+    private Label courseLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        studentsListView.getItems().addAll(DBUtility.getStudentsFromDB());
+        DBUtility.getProfessorsFromDB();
+
+        //update the label's to show how many students and proffesor  are in each list
+        studentsLabel.setText("Students : "+studentListView.getItems().size());
+    }
 }
--- src/utilities/DBUtility.java
+++ src/utilities/DBUtility.java
@@ -1,6 +1,11 @@
 package utilities;
 
+import models.Professor;
+import models.Student;
+
 import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,5 +21,25 @@ public class DBUtility {
             "COMP 1098", "COMP 2003", "ENTR 1002", "COMP 1009",
             "COMP 1011", "COMP 1073", "COMP 2084", "COMP 3002",
             "COMP 2068");
+    }
+
+    public static ArrayList<Student> getStudentsFromDB() {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Rose","Ruffner","3846 St. Paul StreetSt Catharines ON L2S 3A1", LocalDate.of(1975,8,27), 20000001));
+        students.add(new Student("Jack","Bradbury","867 rue des Églises Est Ste Cecile De Masham QC J0X 2W0", LocalDate.of(1979,10,14), 20000002));
+        students.add(new Student("Elanore","Sanders","1145 47th Avenue Grassland AB T0A 1V0", LocalDate.of(1940,9,25), 20000003));
+        students.add(new Student("Nancy","Walsh","1459 Harvest Moon Dr Unionville ON L3R 0L7", LocalDate.of(1999,1,12), 20000004));
+        students.add(new Student("Greta","Tolbert","642 Front Street Toronto ON M5J 2N1", LocalDate.of(1957,12,18),20000005));
+        students.add(new Student("Barbara","Gable","3671 Scotchmere Dr Sarnia ON N7T 7T9", LocalDate.of(2002,11,13),20000006));
+        return students;
+    }
+    public static ArrayList<Professor> getProfessorsFromDB()
+    {
+        ArrayList<Professor> professors = new ArrayList<>();
+        professors.add(new Professor("Lois","Parker","674 Goyeau Ave Windsor ON N9A 1H9",LocalDate.of(1987,3,21),10100));
+        professors.add(new Professor("Ginger","Harris","3514 Yonge Street Toronto ON M4W 1J7",LocalDate.of(1967,11,12),10101));
+        professors.add(new Professor("Winchester","Solomon","3099 Balmy Beach Road Owen Sound ON N4K 2N7",LocalDate.of(1977,12,18),10102));
+        professors.add(new Professor("John","Pressley","1101 Eglinton Avenue Toronto ON M4P 1A6",LocalDate.of(1973,11,29),10103));
+        return professors;
     }
 }
